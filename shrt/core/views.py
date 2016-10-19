8a9ea00ea67ff567ee52176a6bed19fb31833d60
tmnkv <at@idaproject.com>
2016-10-19 17:51:40 +0300

    ADDED view for creatiing new short url

--- shrt/core/views.py
+++ shrt/core/views.py
@@ -1,7 +1,35 @@
-from django.views.generic import TemplateView
+from random import SystemRandom, choice
+from string import ascii_letters as letters
+from string import digits
+
+from django.views.generic import TemplateView, CreateView
+from django.http import JsonResponse
 
 from core.models import URL
+from core.forms import UrlCreateForm
+from core.mixins import InvalidMixin
 
 
 class IndexView(TemplateView):
     template_name = 'core/index.html'
+
+
+class UrlCreateView(InvalidMixin, CreateView):
+    model = URL
+    form_class = UrlCreateForm
+
+    def form_valid(self, form):
+        primary_url = form.cleaned_data['primary']
+        url = URL.objects.create()
+        url.primary = primary_url
+        url.shorten = self.generate_short_link()
+        url.save()
+        return JsonResponse({
+            'shorten' : url.shorten
+        })
+
+    def generate_short_link(self):
+        link = 'http://sh.idaproject.com/' + ''.join(SystemRandom().choice(letters + digits) for _ in range(7))
+        if URL.objects.filter(shorten=link).exists():
+            link = generate_short_link()
+        return link
