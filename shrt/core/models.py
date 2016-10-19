da7ba6b0a90fd045500a9997a0fcc48e7a11b911
tmnkv <at@idaproject.com>
2016-10-19 12:04:08 +0300

    CREATE URL model

--- shrt/core/models.py
+++ shrt/core/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+
+class URL(models.Model):
+    created = models.DateTimeField(
+        default=now,
+        verbose_name='Время и дата создания',
+        editable=False
+    )
+
+    primary= models.URLField(
+        verbose_name='Начальный URL'
+    )
+
+    shorten= models.URLField(
+        verbose_name='Сокращенный URL'
+    )
+
+    counter = models.PositiveIntegerField(
+        default=0,
+        verbose_name='Количество переходов'
+    )
+
+    def counter_plus(self):
+        self.counter += 1
+
+    def __str__(self):
+        return '{0} => {1}'.format(
+            self.shorten,
+            self.primary
+        )
+
+
+    class Meta:
+        verbose_name = 'URL'
+        verbose_name_plural = 'URLs'
