a9561d1df311a989e323a81343491513bbd3199d
tmnkv <at@idaproject.com>
2016-10-19 17:56:15 +0300

    ADDED function for submit form

--- shrt/static/js/main.js
+++ shrt/static/js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
-
+    $('#make-short').on('submit', function (event) {
+        event.preventDefault();
+        console.log('click');
+        $.post('/add/', { primary : $('#long_url').val()}, function (data) {
+            console.log(data);
+            $('#long_url').val('');
+            $('.result__url').text(data.shorten);
+        })
+    })
 });
