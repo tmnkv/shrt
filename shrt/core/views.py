d54a445ba48da740b95e9a3931f4bdc871fc05ff
tmnkv <at@idaproject.com>
2016-10-19 14:16:23 +0300

    ADDED view for index page

--- shrt/core/views.py
+++ shrt/core/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from core.models import URL
+
+
+class IndexView(TemplateView):
+    template_name = 'core/index.html'
