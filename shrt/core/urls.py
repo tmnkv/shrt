0109fdda7fec28bb8bbfedd2faacb7a9141a9438
tmnkv <at@idaproject.com>
2016-10-20 18:58:11 +0300

    ADDED url rule for redirectig from shorten to primary

--- shrt/core/urls.py
+++ shrt/core/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from core.views import IndexView, UrlCreateView
+from core.views import IndexView, UrlCreateView, UrlRedirectView
 
 
 urlpatterns = [
@@ -13,5 +13,10 @@ urlpatterns = [
         regex=r'^add/$',
         view=UrlCreateView.as_view(),
         name='url_add'
+    ),
+    url(
+        regex=r'^(?P<url>\w{7})/$',
+        view=UrlRedirectView.as_view(),
+        name='url_redirect'
     )
 ]
