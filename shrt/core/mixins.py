3a8e78f354a3f2d3f2ee2186a406acb5ddfa8219
tmnkv <tmnkv@ya.ru>
2016-10-21 16:17:54 +0300

    ADDED mixin for email sending

--- shrt/core/mixins.py
+++ shrt/core/mixins.py
@@ -1,8 +1,17 @@
 from django.http import JsonResponse
+from django.template.loader import render_to_string
+
+from core.tasks import send_email_task
 
 
-class InvalidMixin():
-
+class InvalidMixin:
     def form_invalid(self, form):
-        print(form)
         return JsonResponse(form.errors, status=404)
+
+
+class SendEmailMixin:
+    def send_mail(self, subject, template, obj):
+        print("I'm in")
+        message = render_to_string(template, {'obj': obj})
+        print(message)
+        send_email_task.delay(subject, message)
