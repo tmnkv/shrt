2e4da15cef95606fd3ee5ce01b43d38a6cc5463d
tmnkv <tmnkv@ya.ru>
2016-10-24 08:48:49 +0300

    FIX Celery application name

--- shrt/config/celery.py
+++ shrt/config/celery.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from celery import Celery
 
-app = Celery('celery')
+app = Celery('shrt')
 
 app.config_from_envvar('DJANGO_SETTINGS_MODULE')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
